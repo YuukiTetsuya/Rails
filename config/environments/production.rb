6d8749b2732941384a9625af320d85645921aa11
YUUKI <yuukitetsuyanet@gmail.com>
2019-01-25 08:36:59 +0000

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME']
-    :password       => ENV['SENDGRID_PASSWORD']
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttle_auto => true
   }
