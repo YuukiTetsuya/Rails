bfca6638c308e688108b521ca95b21adb747f01d
YUUKI <yuukitetsuyanet@gmail.com>
2019-01-28 16:14:34 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
   root 'static_pages#home'
   get     '/help',    to: 'static_pages#help'
   get     '/about',   to: 'static_pages#about'
@@ -12,6 +8,7 @@ Rails.application.routes.draw do
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
-  resources :users                                      # usersリソースをRESTfullな構造にするためのコード。
-  resources :account_activations, only: [:edit]         # editアクションのみaccount_activationsリソースを適用
+  resources :users                                                              # usersリソースをRESTfullな構造にするためのコード。
+  resources :account_activations, only: [:edit]                                 # editアクションのみaccount_activationsリソースを適用
+  resources :password_resets,     only: [:new, :create, :edit, :update]         # password再設定用のリソースを適用
 end
